483c51a2c2c2dc02bd99ae0110bc3719a9832052
Sangmin Oh <ohsm1226@naver.com>
2022-06-01 16:21:51 +0900

    SoundManager 추가

--- YouDriveMeCrazy/Assets/Scripts/GamePlay/SoundManager.cs
+++ YouDriveMeCrazy/Assets/Scripts/GamePlay/SoundManager.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public enum Type { all = 0, bgm, gameSfx, carSfx, obstacleSfx }
+public enum BGM { titleBgm = 0, waitingRoomBgm, stageBgm };
+public enum GameSfx { gameFail = 0, stageClear, enterClearZone };
+public enum CarSfx { carKlaxon = 0, carBreak, carWiper, startEngine };
+public enum ObstacleSfx { warning = 0, animalRunaway, dirt, hitCar };
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
-
-    public enum Type {all=0, bgm, gameSfx, carSfx, obstacleSfx}
-    public enum BGM { titleBgm = 0, waitingRoomBgm, stageBgm };
-    public enum GameSfx { gameFail = 0, stageClear, enterClearZone };
-    public enum CarSfx { carKlaxon = 0, carBreak, carWiper, startEngine };
-    public enum ObstacleSfx { warning = 0, animalRunaway, dirt, hitCar };
 
     [Header("Speaker")]
     #region Speaker
@@ -31,38 +31,47 @@ public class SoundManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            //에러나면 밑에 지우기
-            DontDestroyOnLoad(this);
         }
         else
         {
-            Destroy(gameObject);
+            if(Instance != this){
+                Destroy(this.gameObject);
+            }
         }
     }
 
-    void PlayBgm(BGM bgmNum)
+    private void Start() {
+        if(SceneManager.GetActiveScene().buildIndex == 0){
+            print("hello");
+        }
+        else{
+            print("hello2");
+        }
+    }
+
+    public void PlayBgm(BGM bgmNum)
     {
         bgmSpeaker.loop = true;
         bgmSpeaker.PlayOneShot(bgm[(int)bgmNum]);
     }
-    void PlayGameSfx(GameSfx sfxNum)
+    public void PlayGameSfx(GameSfx sfxNum)
     {
         gameSfxSpeaker.loop = false;
         gameSfxSpeaker.PlayOneShot(gameSfx[(int)sfxNum]);
     }
 
-    void PlayCarSfx(CarSfx sfxNum)
+    public void PlayCarSfx(CarSfx sfxNum)
     {
         carSfxSpeaker.loop = false;
         carSfxSpeaker.PlayOneShot(carSfx[(int)sfxNum]);
     }
-    void PlayObstacleSfx(ObstacleSfx sfxNum)
+    public void PlayObstacleSfx(ObstacleSfx sfxNum)
     {
         obstacleSfxSpeaker.loop = false;
         obstacleSfxSpeaker.PlayOneShot(obstacleSfx[(int)sfxNum]);
     }
 
-    void Stop(Type type)
+    public void Stop(Type type)
     {
         switch (type)
         {
